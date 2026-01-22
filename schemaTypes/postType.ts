61cb611a9b74b6bbe9cb0526a5160ca72a5982ab
Marian Noekel <mariannoekel@gmail.com>
2026-01-22 10:10:54 +0100

    initial commit

---
+++ schemaTypes/postType.ts
@@ -0,0 +1,42 @@
+import {defineField, defineType} from 'sanity'
+
+export const postType = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'seoTitle',
+      title: 'SEO Title Tag',
+      type: 'string',
+      description: 'Dieser Titel erscheint im Browser-Tab und bei Google. (Ideal: 50-60 Zeichen)',
+      validation: Rule => Rule.max(70).warning('Der Titel ist etwas zu lang für Google.')
+    })
+  ],
+})
